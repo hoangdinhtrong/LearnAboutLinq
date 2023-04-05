3e7e37eaa2e849ef301b4a4f78ffb158de5904ee
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-04-05 10:59:04 +0700

    Learn about Select Extension in Linq

---
+++ LinqInternals.Demo/LinqInternals.Demo/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqInternals.Demo.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        /// <summary>
+        /// WHERE EXTENSION IN LINQ
+        /// </summary>
+        public static IEnumerable<T> NewWhere<T>(this IEnumerable<T> items,
+            Func<T, bool> predicate)
+        {
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+
+            //// SOLUTION 2
+            //List<T> list = new List<T>();
+            //foreach (var item in items)
+            //{
+            //    if (predicate(item))
+            //    {
+            //        list.Add(item);
+            //    }
+            //}
+            //return list;
+        }
+
+        /// <summary>
+        /// SELECT EXTENSION IN LINQ
+        /// </summary>
+        public static IEnumerable<TResult> NewSelect<T, TResult>(this IEnumerable<T> items,
+            Func<T, TResult> selector)
+        {
+            foreach (var item in items)
+            {
+                yield return selector(item);
+            }
+        }
+
+        /// <summary>
+        /// SELECT MANY EXTENSION IN LINQ
+        /// </summary>
+        public static IEnumerable<TResult> NewManySelect<T, TResult>(this IEnumerable<T> items,
+            Func<T, IEnumerable<TResult>> selector)
+        {
+            foreach (var item in items)
+            {
+                foreach(var innerItem in selector(item))
+                {
+                    yield return innerItem;
+                }
+            }
+        }
+    }
+}
--- LinqInternals.Demo/LinqInternals.Demo/Program.cs
+++ LinqInternals.Demo/LinqInternals.Demo/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace LinqInternals.Demo
+﻿using LinqInternals.Demo.Extensions;
+using LinqInternals.Demo.Models;
+
+namespace LinqInternals.Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            ExampleWhereLinq();
+            Customer[]? customers = new[]
+            {
+                new Customer()
+                {
+                    Name = "Foo",
+                    Phones = new[]
+                    {
+                        new Phone(){ Number = "123", PhoneType = PhoneType.Home },
+                        new Phone(){ Number = "456", PhoneType = PhoneType.Cell },
+                    }
+                },
+                new Customer()
+                {
+                    Name = "Foo 2",
+                    Phones = new[]
+                    {
+                        new Phone(){ Number = "345-345-3456", PhoneType = PhoneType.Cell},
+                        new Phone(){ Number = "456-678-5678", PhoneType = PhoneType.Home},
+                    }
+                }
+            };
+            ExampleSelectManyLinq(customers);
+        }
+
+        private static void ExampleSelectLinq(IEnumerable<Customer> customers)
+        {
+
+            if (customers != null && customers.Count() > 0)
+            {
+                var customerNames = customers.NewSelect(x => x.Name);
+                foreach (var name in customerNames)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+
+        private static void ExampleSelectManyLinq(IEnumerable<Customer> customers)
+        {
+
+            if (customers != null && customers.Count() > 0)
+            {
+                var phones = customers.NewManySelect(x =>x.Phones);
+                foreach (var item in phones)
+                {
+                    Console.WriteLine($"{item.Number} {item.PhoneType}");
+                }
+            }
         }
 
         private static void ExampleWhereLinq()
